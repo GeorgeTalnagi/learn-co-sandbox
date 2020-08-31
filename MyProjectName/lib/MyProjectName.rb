2d718f51c7b8d20f52d53a1050ad0ae11a816ad1
George Talnagi <gtalnagi@yahoo.com>
2020-08-31 23:05:04 +0000

    Automatically backed up by Learn

---
+++ MyProjectName/lib/MyProjectName.rb
@@ -0,0 +1,6 @@
+require "MyProjectName/version"
+
+module MyProjectName
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ MyProjectName/lib/MyProjectName/version.rb
@@ -0,0 +1,3 @@
+module MyProjectName
+  VERSION = "0.1.0"
+end
